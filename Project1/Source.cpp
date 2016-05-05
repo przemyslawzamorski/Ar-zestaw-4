6844caeead3fb7dc07504ac20d5be78469b3ec2c
Zamor <przemyslaw.zamorskii@gmail.com>
2016-05-05 23:59:37 +0200

    dziala scatter v

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -38,26 +38,43 @@ int main(int argc, char **argv) {
 	MPI_Type_commit(&barDatatype);
 
 
+	int pixel_count;
+	int arr_dimensions[2];
+	BMP Input;
+
+	int* displs = new int[size]; /* tablica przesuniecia */
 
 	if (rank == 0){
-		/*wczytanie na bitmapy */
+		/*wczytanie na bitmapy i rozeslanie wymiarow */
 		char *BITMAP_SCR = "test.BMP";
-		BMP Input;
+		
 
 		Input.ReadFromFile(BITMAP_SCR);
-		int width = Input.TellWidth();
+		
 		int height = Input.TellHeight();
+		int width = Input.TellWidth();
 
-		int pixel_count = width*height;
+		arr_dimensions[0] = height;
+		arr_dimensions[1] = width;
+		MPI_Bcast(arr_dimensions, 2, MPI_INT, 0, MPI_COMM_WORLD);
+	}
+	else{
+		MPI_Bcast(arr_dimensions, 2, MPI_INT, 0, MPI_COMM_WORLD);
+	}
 
-		Color* buffer = (Color*)malloc(sizeof(Color)*pixel_count);
+	pixel_count = arr_dimensions[1] * arr_dimensions[0];
+		
 
+	Color* buffer = (Color*)malloc(sizeof(Color)*pixel_count);
+
+
+	if (rank == 0){
 		int counter = 0;
 
 		/*wczytanie bitmapy do tablicy struktor*/
-		for (int i = 0; i<height; ++i)
+		for (int i = 0; i<arr_dimensions[0]; ++i)
 		{
-			for (int j = 0; j<width; ++j)
+			for (int j = 0; j<arr_dimensions[1]; ++j)
 			{
 				buffer[counter].r = Input(j, i)->Red;
 				buffer[counter].g = Input(j, i)->Green;
@@ -71,7 +88,7 @@ int main(int argc, char **argv) {
 
 		int default_for_process = pixel_count / size; /*domyslnie na proces*/
 		int rest = pixel_count % size; /*reszta do podzielenia miedzy procesy*/
-		int* displs = new int[size]; /* tablica przesuniecia */
+		
 		int sum = 0; // suma zliczen. uzywana to obliczenia przesuniecia
 
 
@@ -86,6 +103,7 @@ int main(int argc, char **argv) {
 
 		/*rozeslanie po ile dla kazdego procesu */
 		MPI_Bcast(per_process, size, MPI_INT, 0, MPI_COMM_WORLD);
+		MPI_Bcast(displs, size, MPI_INT, 0, MPI_COMM_WORLD);
 		cout << rank << " " << per_process[rank] << endl;
 
 
@@ -96,6 +114,7 @@ int main(int argc, char **argv) {
 		MPI_Bcast(per_process, size, MPI_INT, 0, MPI_COMM_WORLD);
 		cout << rank << " " << per_process[rank] << endl;
 		Color* buffer = (Color*)malloc(sizeof(Color)*10);
+		MPI_Bcast(displs, size, MPI_INT, 0, MPI_COMM_WORLD);
 
 
 	}
@@ -104,7 +123,7 @@ int main(int argc, char **argv) {
 	/*stworzenie tablicy pikseli przydzielonych po podziale  */
 	Color* pixel_per_process = (Color*)malloc(sizeof(Color)*per_process[rank]);
 
-	MPI_Scatterv(&buffer, per_process, displs, barDatatype, pixel_per_process, per_process[rank], barDatatype, 0, MPI_COMM_WORLD);
+	MPI_Scatterv(buffer, per_process, displs, barDatatype, pixel_per_process, per_process[rank], barDatatype, 0, MPI_COMM_WORLD);
 
 
 	for(int i = 0; i < per_process[rank]; i++) {
@@ -115,48 +134,6 @@ int main(int argc, char **argv) {
 
 
 
-
-	/*obliczenie luminacji kazdego pixelu*/
-	int lumination_sum = 0;
-
-	for (int i = 0; i < per_process[rank]; i++) {
-
-		pixel_per_process[i].i = 0, 299 * pixel_per_process[i].r + 0, 587 * pixel_per_process[i].g + 0, 114 * pixel_per_process[i].b;
-		lumination_sum += pixel_per_process[i].i;
-	}
-
-	/*obliczenie sr luminacji i rozeslanie do procesow*/
-	int sr_lumination = 0;
-
-	MPI_Reduce(&lumination_sum, &sr_lumination, 1, MPI_INT, MPI_SUM, 0 /*root*/, MPI_COMM_WORLD);
-
-	sr_lumination = sr_lumination / pixel_count;
-
-	MPI_Bcast(&sr_lumination, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
-	/*zmienianie koloru*/
-
-	for (int i = 0; i < per_process[rank]; i++) {
-
-		if (pixel_per_process[i].i > sr_lumination){
-			pixel_per_process[i].r = 255;
-			pixel_per_process[i].g = 255;
-			pixel_per_process[i].b = 255;
-
-		}
-		else{
-			pixel_per_process[i].r = 0;
-			pixel_per_process[i].g = 0;
-			pixel_per_process[i].b = 0;
-		}
-	}
-
-	/*zebranie wszystkich zamienionych*/
-
-	Color* buffer_recv = (Color*)malloc(sizeof(Color)*pixel_count);
-
-	MPI_Gatherv(pixel_per_process, per_process[rank], barDatatype, buffer_recv, pixel_count, displs, barDatatype,
-		0, MPI_COMM_WORLD);
 
 
 
