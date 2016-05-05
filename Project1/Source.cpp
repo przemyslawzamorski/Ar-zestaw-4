e329ea4c7468e24746f0b0c003851a4c1b6260f8
Zamor <przemyslaw.zamorskii@gmail.com>
2016-05-05 04:54:18 +0200

    wczytanie bitmapy

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int main(int argc, char **argv) {
 
-	char *BITMAP_SCR = "MARBLES.BMP";
+	
 
 	MPI_Init(&argc, &argv);
 	int rank, size;
@@ -17,17 +17,32 @@ int main(int argc, char **argv) {
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-	/*inicjalizacja procesu  ile na proces */
+	/*inicjalizacja procesu  ile na proces i zerowanie */
 	int* per_process = new int[size];
 
 	for (int i = 0; i < size; i++){
 			per_process[i] = 0;
 		}
 
+
+
+
+
+
 	if (rank == 0){
+		/*wczytanie pliku do tablicy i znalezienie get width and height */
+		char *BITMAP_SCR = "test.BMP";
+		BMP Input;
+
+		Input.ReadFromFile(BITMAP_SCR);
+		int width = Input.TellWidth();
+		int height = Input.TellHeight();
+
+		cout << width << endl;
+		cout << height << endl;
 		
 
-		for (int i = 0; i < size; i++){
+		/*for (int i = 0; i < size; i++){
 			int default_for_process = 12 / size;
 			int rest = 12 % size;
 
@@ -36,18 +51,18 @@ int main(int argc, char **argv) {
 				
 			}
 			per_process[i] = default_for_process;
-		}
+		}*/
 
 	}
 
 
-	MPI_Bcast(per_process, size, MPI_INT, 0, MPI_COMM_WORLD);
+	/*MPI_Bcast(per_process, size, MPI_INT, 0, MPI_COMM_WORLD);
 
 
 	
 		cout << rank << " " << per_process[rank] << endl;
 	
-	cout << endl;
+	cout << endl;*/
 	
 	MPI_Finalize();
 	return 0;
