b9b0d25f98046023b3b2663a7e48a17c5b3b5168
Zamor <przemyslaw.zamorskii@gmail.com>
2016-05-05 19:55:19 +0200

    przed dodaniem struct

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -9,25 +9,13 @@ using namespace std;
 
 int main(int argc, char **argv) {
 
-	
+
 
 	MPI_Init(&argc, &argv);
 	int rank, size;
 
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-	/*inicjalizacja procesu  ile na proces i zerowanie */
-	int* per_process = new int[size];
-
-	for (int i = 0; i < size; i++){
-			per_process[i] = 0;
-		}
-
-
-
-
-
 
 	if (rank == 0){
 		/*wczytanie pliku do tablicy i znalezienie get width and height */
@@ -43,64 +31,30 @@ int main(int argc, char **argv) {
 
 		int pixel_count = width*height;
 
-		/*counter = 0
-			dla height 
-				dla width
-				ary[counter][0]=rgb
-				ary[counter][1]= rgb
-				ary[counter][2]= rgb
-				przypisywanie do tablicy poprzez wskaŸnik 
-				
-				*/
-		// dynamic allocation
-		int** ary = new int*[pixel_count];
+		int ** ary = (int**)malloc(pixel_count * sizeof(int*));
 		for (int i = 0; i < pixel_count; ++i)
-			ary[i] = new int[3];
-
-
-	
-
+			ary[i] = (int*)malloc(3 * sizeof(int));
 
 
 		// fill
-		for (int i = 0; i < pixel_count; ++i)
-			for (int j = 0; j < 3; ++j)
-				ary[i][j] = 5;
+		for (int i = 0; i < pixel_count; ++i){
+				ary[i][1] = 5;
+				ary[i][0] = 5;
+				ary[i][2] = 5;
+		}
 
 		// print
 		for (int i = 0; i < pixel_count; ++i){
 			cout << endl;
 			for (int j = 0; j < 3; ++j)
-				std::cout << i<<" "<<j<<" "<< ary[i][j] << "\n";
+				cout << i << " " << j << " " << ary[i][j] << "\n";
 
 		}
 		
 		
 
-		
-
-		/*for (int i = 0; i < size; i++){
-			int default_for_process = 12 / size;
-			int rest = 12 % size;
-
-			if (i < rest){
-				default_for_process++;
-				
-			}
-			per_process[i] = default_for_process;
-		}*/
 
 	}
-
-
-	/*MPI_Bcast(per_process, size, MPI_INT, 0, MPI_COMM_WORLD);
-
-
-	
-		cout << rank << " " << per_process[rank] << endl;
-	
-	cout << endl;*/
-	
-	MPI_Finalize();
-	return 0;
-}
+		MPI_Finalize();
+		return 0;
+	}
